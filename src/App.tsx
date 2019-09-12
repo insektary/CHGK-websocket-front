97386ce2424e767b84a7f8c388e01c5ea2aca109
Maxim Dashkov <insektarty@mail.ru>
2019-09-12 21:35:14 +0300

    to public

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,54 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    componentDidMount() {
+        console.log('mount and send query');
+
+        const socket = new WebSocket('ws://localhost');
+
+        socket.onopen = () => {
+            console.log('Соединение установлено');
+            socket.send('test message');
+        };
+          
+        socket.onclose = (event) => {
+            if (event.wasClean) {
+                console.log('Соединение закрыто чисто');
+            } else {
+                console.log('Обрыв соединения');
+            }
+            console.log('Код: ' + event.code + ' причина: ' + event.reason);
+        };
+          
+        socket.onmessage = (event) => {
+            console.log('Получены данные ' + event.data);
+        };
+          
+        socket.onerror = (error) => {
+            console.log('Ошибка ');
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
